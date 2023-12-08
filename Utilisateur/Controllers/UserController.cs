b7735a503374484a2af8bd21f1f84e13c0ce57f7
xerneas02 <mathis.roubille@gmail.com>
2023-12-08 15:08:08 +0100

    Ajoutez des fichiers projet.

---
+++ Utilisateur/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using utilisateur.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace utilisateur.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Entities.Utilisateur>>> GetUtilisateurs()
+        {
+            return Ok(await _context.Utilisateurs.ToListAsync());
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Entities.Utilisateur>> GetUtilisateur(int id)
+        {
+             return Ok(await _context.Utilisateurs.FirstOrDefaultAsync(user => user.Id == id));
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<ActionResult<Entities.Utilisateur>> PostUtilisateur(Entities.Utilisateur utilisateur)
+        {
+            // On ajoute notre utilisateur dans la base
+            _context.Utilisateurs.Add(utilisateur);
+            // On sauvegarde la modification
+            await _context.SaveChangesAsync();
+            // On retourne l'utilisateur nouvellement crée en appelant la fonction CreatedAtAction
+            return CreatedAtAction(nameof(GetUtilisateur), new { id = utilisateur.Id },utilisateur);
+        }
+    }
+}
---
+++ Utilisateur/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace utilisateur.Data
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DataContext(IConfiguration configuration)
+        {
+
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Connexion a la base sqlite
+            options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+        // On déclare que notre base aura une table Utilisateurs qui contiendra des objets de type Utilisateur
+        public DbSet<Entities.Utilisateur> Utilisateurs { get; set; }
+    }
+}
